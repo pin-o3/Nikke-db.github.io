60a937280315024fe699190a30644f0be84013b7
Koshirei <axelaxelc@aol.fr>
2022-05-08 23:11:33 +0200

    spine start

---
+++ js/visualiser.js
@@ -0,0 +1,9 @@
+new spine.SpinePlayer("player-container", {
+      jsonUrl: "/l2d/c012/c012_00.json",
+      atlasUrl: "/l2d/c012/c012_00.atlas",
+      animation: "idle",
+      skin: "00",
+      skins: ["00"],
+      backgroundColor: "#00000000",
+      alpha: true
+});
