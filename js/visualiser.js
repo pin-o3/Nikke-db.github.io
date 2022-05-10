9d339d0c52e200c91884d2bd903edbe7a7215509
Koshirei <axelaxelc@aol.fr>
2022-05-10 21:34:54 +0200

    fix listeners

--- js/visualiser.js
+++ js/visualiser.js
@@ -33,6 +33,8 @@ const changeSpine = (id) => {
       // every listeners MUST be in changeSpine because
       // there are any spine currently, so the listened divs
       // doesn't exist, thus will break the code and nothing will really work
+      
+      
 
       document.querySelector("#player-container").innerHTML = " "
 
@@ -48,79 +50,80 @@ const changeSpine = (id) => {
 
       document.querySelector(".spine-player-canvas").width = document.querySelector(".spine-player-canvas").height
 
-      document.addEventListener("wheel", (e) => {
-       
-            if (e.target !== document.querySelector("#background-div") &&
-                e.target !== document.querySelector(".spine-player-canvas")){
-                      return false
-                }
-             canvas = document.querySelector("#player-container")
-             height = canvas.style.height.replaceAll("vh","")
-         
-             switch(e.deltaY>0){
-                   case true:   
-                   if (parseInt(canvas.style.height.replaceAll("vh",""))<=20) return false 
-                   canvas.style.height = parseInt(height) - 5 +"vh" ;
-                   break;
-                   case false:  
-                   if (parseInt(canvas.style.height.replaceAll("vh",""))>=500) return false 
-                   canvas.style.height = parseInt(height) + 5 +"vh"     ;
-                   break;
-             }
-       })
+      
       
       document.querySelector(".spine-player-canvas").style.width = null
       
       document.querySelector(".spine-player-canvas").style.display = "inline"
       
-      let move = false
-      let oldx = "";
-      let oldy = "";
       
-      document.addEventListener("mousedown", (e) => {
-            if (e.target !== document.querySelector("#background-div") &&
-                e.target !== document.querySelector(".spine-player-canvas")){
-                      return false
-                }
-            move = true
-            oldx = e.clientX;
-            oldy = e.clientY;
-      })
-      document.addEventListener("mouseup", (e) => {
-            oldx = ""
-            oldy = ""
-            move = false
-      })
       
-      document.addEventListener("mousemove", (e) => {
-            if (move) {
-                  
-                  let newx = e.clientX
-                  let newy = e.clientY
-                  let stylel;
-      
-                  stylel = document.querySelector("#player-container").style.left.replaceAll("px", "")
-      
-                  let stylet = document.querySelector("#player-container").style.top.replaceAll("px", "")
-                  if (newx > oldx) {
-                        document.querySelector("#player-container").style.left = (parseInt(stylel) + (newx - oldx)) + "px"
-                  }
-                  if (newx < oldx) {
-                        document.querySelector("#player-container").style.left = (parseInt(stylel) + (newx - oldx)) + "px"
-                  }
-                  if (newy < oldy) {
-                        document.querySelector("#player-container").style.top = (parseInt(stylet) + (newy - oldy)) + "px"
-                  }
-                  if (newy > oldy) {
-                        document.querySelector("#player-container").style.top = (parseInt(stylet) + (newy - oldy)) + "px"
-                  }
-                  oldx = newx
-                  oldy = newy
-            }
-      }
-      )
 }
 
+let move = false
+let oldx = "";
+let oldy = "";
 
+document.addEventListener("wheel", (e) => {
+       
+      if (e.target !== document.querySelector("#background-div") &&
+          e.target !== document.querySelector(".spine-player-canvas")){
+                return false
+          }
+       canvas = document.querySelector("#player-container")
+       height = canvas.style.height.replaceAll("vh","")
+   
+       switch(e.deltaY>0){
+             case true:   
+             if (parseInt(canvas.style.height.replaceAll("vh",""))<=20) return false 
+             canvas.style.height = parseInt(height) - 5 +"vh" ;
+             break;
+             case false:  
+             if (parseInt(canvas.style.height.replaceAll("vh",""))>=500) return false 
+             canvas.style.height = parseInt(height) + 5 +"vh"     ;
+             break;
+       }
+ })
 
+document.addEventListener("mousedown", (e) => {
+      if (e.target !== document.querySelector("#background-div") &&
+          e.target !== document.querySelector(".spine-player-canvas")){
+                return false
+          }
+      move = true
+      oldx = e.clientX;
+      oldy = e.clientY;
+})
+document.addEventListener("mouseup", (e) => {
+      oldx = ""
+      oldy = ""
+      move = false
+})
 
+document.addEventListener("mousemove", (e) => {
+      if (move) {
+            
+            let newx = e.clientX
+            let newy = e.clientY
+            let stylel;
+
+            stylel = document.querySelector("#player-container").style.left.replaceAll("px", "")
+
+            let stylet = document.querySelector("#player-container").style.top.replaceAll("px", "")
+            if (newx > oldx) {
+                  document.querySelector("#player-container").style.left = (parseInt(stylel) + (newx - oldx)) + "px"
+            }
+            if (newx < oldx) {
+                  document.querySelector("#player-container").style.left = (parseInt(stylel) + (newx - oldx)) + "px"
+            }
+            if (newy < oldy) {
+                  document.querySelector("#player-container").style.top = (parseInt(stylet) + (newy - oldy)) + "px"
+            }
+            if (newy > oldy) {
+                  document.querySelector("#player-container").style.top = (parseInt(stylet) + (newy - oldy)) + "px"
+            }
+            oldx = newx
+            oldy = newy
+      }
+}
+)
